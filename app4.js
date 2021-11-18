3e4dd3ceeddcb2aa0a84740808d58b30d9c9f772
luis garcia <luigy-antoni@outlook.com>
2021-11-17 23:22:25 -0600

    Entregable clase 4 reordenado

--- app4.js
+++ app4.js
@@ -1,16 +1,18 @@
-function calculadora(numUno, numDos, operacion) {
+function calculadora(operacion, numUno, numDos) {
+    let var1 = parseInt(numUno);
+    let var2 = parseInt(numDos);
     switch (operacion) {
         case "1":
-            return numUno + numDos;
+            return var1 + var2;
             break;
         case "2":
-            return numUno - numDos;
+            return var1 - var2;
             break;
         case "3":
-            return numUno * numDos;
+            return var1 * var2;
             break;
         case "4":
-            return numUno / numDos;
+            return var1 / var2;
             break;
         default:
             return 0;
@@ -18,5 +20,5 @@ function calculadora(numUno, numDos, operacion) {
     }
 }
 
-let resultado = calculadora(prompt("Ingresa el primer número"), prompt("Ingresa el segundo número"), prompt("Escribe el número correspondiente a la operación a realizar: (1)Suma (2)Resta (3)Multiplicación (4)División"));
+let resultado = calculadora(prompt("Escribe el número correspondiente a la operación a realizar: (1)Suma (2)Resta (3)Multiplicación (4)División"), prompt("Ingresa el primer número"), prompt("Ingresa el segundo número"));
 console.log(resultado);
